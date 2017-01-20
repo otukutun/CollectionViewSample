0f604893b2c0a28fd1cf97adee1229e8b3fa3e77
otukutun <orehaorz@gmail.com>
2017-01-20 13:11:12 +0900

    一旦UICollectionVIewを使って表示できた.

--- CollectionViewSample/ViewController.swift
+++ CollectionViewSample/ViewController.swift
@@ -8,10 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    let imageList:Array<String> = ["bob.png", "bob2.png", "dora.png", "dora2.png", "dora3.png", "nobi.png", "otukutun.png"]
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +25,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imageList.count
+    }
 
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let collectionView = collectionView.dequeueReusableCell(withReuseIdentifier: "TestCell", for: indexPath) as UICollectionViewCell
+        collectionView.backgroundColor = UIColor(red: CGFloat(drand48()),
+                                       green: CGFloat(drand48()),
+                                       blue: CGFloat(drand48()),
+                                       alpha: 1.0)
+        return collectionView
+    }
 }
 
